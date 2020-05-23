c4f219bed8e357ebc887419082ddaf4cc0c55826
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 21:06:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,21 +11,13 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
   end
+  
+  extend Memorable, Findable
+  
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def add_song(song)
     @songs << song
@@ -36,7 +28,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,12 @@
+# require_relative "../artist.rb"
+module Memorable
+  
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+end
